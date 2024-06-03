335d750c8c1e71081578d7518629b0097e1b5dd6
Edi <you@example.com>
2024-06-03 23:56:12 +0200

    Profiling MailController (our main Controller).

--- MailService/app/Http/Controllers/MailController.php
+++ MailService/app/Http/Controllers/MailController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 class MailController extends Controller
 {
 
-
+    //displaying the inbox with the mails of the authenticated person
     public function inbox(Request $request){
         
         $mails = Mail::where('reciever_id', auth()->user()->id)
@@ -31,19 +30,22 @@ class MailController extends Controller
         return Inertia::render('Inbox', ['mails' => $mails,'currentRoute' => $currentRoute]);
     }
 
+    //displaying the sent with the mails of the authenticated person
     public function sent(Request $request){
 
         
         $mails = Mail::where('sender_id', auth()->user()->id)
              ->where('sender_deleted', 0)
              ->get();
-        $users = User::All();
+        
+             $users = User::All();
 
         $currentRoute = Route::currentRouteName();
        
         return Inertia::render('Sent', ['mails' => $mails, 'users' => $users, 'currentRoute' => $currentRoute]);
     }
 
+    //displaying junk
     public function junk(Request $request){
 
         $mails = Mail::where('junk', true)
@@ -55,6 +57,7 @@ class MailController extends Controller
         return Inertia::render('Junk', ['mails' => $mails, 'currentRoute' => $currentRoute]);
     }
 
+    //displaying trash
     public function trash(Request $request){
         
         $mails = Mail::where('trash', true)
@@ -67,6 +70,7 @@ class MailController extends Controller
         
     }
 
+    //displaying newmail for sending mails
     public function newmail(Request $request){
 
         $currentRoute = Route::currentRouteName();
@@ -74,128 +78,97 @@ class MailController extends Controller
         return Inertia::render('NewMail', ['currentRoute' => $currentRoute]);
     }
 
+    //function for sending mails
     public function addmail(Request $request){
 
-                    $junk_keywords = [
-                        'free',
-                        'click here',
-                        'earn money',
-                        'discount',
-                        'winner',
-                        'investment',
-                        'loan',
-                        'casino',
-                        'limited offer',
-                        'money back',
-                        'risk-free',
-                        'weight loss'
-                    ];
+        $junk_keywords = [
+            'free', 'click here', 'earn money', 'discount', 'winner', 'investment', 
+            'loan', 'casino', 'limited offer', 'money back', 'risk-free', 'weight loss'
+        ];
 
 
-                    $income = $request->validate([
-                        'reciever_mail' => 'required',
-                        'title' => 'required',
-                        'body' => 'required',
-                    ]);
+        $income = $request->validate([
+             'reciever_mail' => 'required',
+             'title' => 'required',
+            'body' => 'required',
+        ]);
 
-                    $email = $request->reciever_mail;
-                    $receiver = User::where('email', $email)->first(); 
+        $receiver = User::where('email', $income['reciever_mail'])->first();
                             
-                    if($receiver) {
-                        $income['reciever_id'] = $receiver->id; 
-                    } else {
-                        return Inertia::render('NewMail');
-                    }
-                    
-                    $income['sender_id'] = Auth()->user()->id;
-                    $income['sender_mail'] = Auth()->user()->email;
+        if (!$receiver)   return Inertia::render('NewMail');
 
-                    //function for junk detection
-                    $message = $request->body;
+        $income['reciever_id'] = $receiver->id;
+        $income['sender_id'] = Auth()->user()->id;
+        $income['sender_mail'] = Auth()->user()->email;
 
-                    $message = strtolower($message);
-                    
-                    foreach($junk_keywords as $keyword) {
-                            if (strpos($message, $keyword) !== false) {
-                                $income['junk'] = true;
-                                break; 
-                            }
-                    }
-
-                   
-
-                    Mail::Create($income);
+        $message = strtolower($income['body']);
         
+        //function for junk detection
+        foreach($junk_keywords as $keyword) {
+                  if (strpos($message, $keyword) !== false) {
+                        $income['junk'] = true;
+                        break; 
+                    }
+        }
+
+        Mail::Create($income);
         
-                return redirect('/newmail');
-                //return Inertia::render('NewMail');
+        return redirect('/newmail');
     }
 
+    // function for delete mail on sender side
     public function delete_sender_mail(Request $request){
         
-
         $mail = Mail::find($request->mailId);
         $mail->sender_deleted = 1;
         $mail->save();
         
         if($mail->sender_deleted && $mail->reciever_deleted && !$mail->trash) $mail->delete();
         
-
         return redirect('/sent');
-
     }
 
+    // function for delete mail on reciever side
     public function delete_reciever_mail(Request $request){
         
-
         $mail = Mail::find($request->mailId);
         $mail->reciever_deleted = 1;
         $mail->save();
         
         if($mail->sender_deleted && $mail->reciever_deleted && !$mail->trash) $mail->delete();
         
-
         return redirect('/inbox');
-
     }
 
-    
+    //function for moving mails to trash
     public function move_to_trash(Request $request){
         
-
         $mail = Mail::find($request->mailId);
         $mail->trash = 1;
         $mail->save();
-        
-        //if($mail->sender_deleted && $mail->reciever_deleted && !$mail->trash) $mail->delete();
-        $mails = Mail::where('trash', true)->get();
 
         return redirect('/inbox');
-        //return Inertia::render('Inbox');
-
     }
 
-    
+    //function for deleting mails from trash
     public function delete_from_trash(Request $request){
         
-
         $mail = Mail::find($request->mailId);
         $mail->trash = 0;
         $mail->reciever_deleted = 1;
         $mail->save();
         
-        //if($mail->sender_deleted && $mail->reciever_deleted && !$mail->trash) $mail->delete();
         $mails = Mail::where('trash', true)
             ->where('reciever_id', Auth::id())
-             ->get();
+            ->get();
 
         if($mail->sender_deleted && $mail->reciever_deleted && !$mail->trash) $mail->delete();
 
-        //return redirect('/trash');
         return Inertia::render('Trash', ['mails' => $mails]);
 
     }
 
+    //function for deleting mails from junk
     public function delete_from_junk(Request $request){
         
         $mail = Mail::find($request->mailId);
@@ -206,9 +179,9 @@ class MailController extends Controller
              ->get();
 
         return Inertia::render('Junk', ['mails' => $mails]);
-
     }
 
+    //function for returning mails to inbox
     public function not_junk(Request $request){
 
         $mail = Mail::find($request->mailId);
@@ -220,68 +193,45 @@ class MailController extends Controller
              ->get();
 
         return Inertia::render('Junk', ['mails' => $mails]);
-
     }
 
+    //function for opening and displaying mails in inbox 
     public function open_mail_inbox(Request $request){
 
-        $income = $request->validate([
-            'id' => 'required',
-        ]);
+        $request->validate(['id' => 'required|integer']);
 
-        $mail_id = intval($request->id);
+        $mail = Mail::findOrFail($request->id);
 
-        $mail = Mail::find($mail_id);
-
-        return Inertia::render('OpenMail', ['mail'=> $mail, 'inbox_or_sent' => true]);
-
+        return Inertia::render('OpenMail', ['mail' => $mail, 'inbox_or_sent' => true]);
     }
 
+    //function for opening and displaying mails in sent and others 
     public function open_mail_sent(Request $request){
 
-        $income = $request->validate([
-            'id' => 'required',
-        ]);
+        $request->validate(['id' => 'required|integer']);
 
-        $mail_id = intval($request->id);
+        $mail = Mail::findOrFail($request->id);
 
-        $mail = Mail::find($mail_id);
-
-        return Inertia::render('OpenMail', ['mail'=> $mail, 'inbox_or_sent' => false]);
-
+        return Inertia::render('OpenMail', ['mail' => $mail, 'inbox_or_sent' => false]);
     }
 
-
-
+    //function for replying mail
     public function reply(Request $request){
 
-        $income = $request->validate([
-            'id' => 'required',
-        ]);
+        $request->validate(['id' => 'required|integer']);
 
-        $mail_id = intval($request->id);
+        $mail = Mail::findOrFail($request->id);
 
-        $mail = Mail::find($mail_id);
-
-
-        return Inertia::render('NewMail', ['mail_reply'=> $mail]);
-
+        return Inertia::render('NewMail', ['mail_reply' => $mail]);
     }
 
+    //function for forwarding mail
     public function forward(Request $request){
 
-        $income = $request->validate([
-            'id' => 'required',
-        ]);
+        $request->validate(['id' => 'required|integer']);
 
-        $mail_id = intval($request->id);
+        $mail = Mail::findOrFail($request->id);
 
-        $mail = Mail::find($mail_id);
-
-
-        return Inertia::render('NewMail', ['mail_forward'=> $mail]);
-
-    }
-
-    
+        return Inertia::render('NewMail', ['mail_forward' => $mail]);
+    }   
 }
